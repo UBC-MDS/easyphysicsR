eb807ba0c06321c3e36715ddc21141d717185e75
Revathy Ponnambalam <revathyp@student.ubc.ca>
2023-01-19 15:39:19 -0800

    updated function and roxygen

---
+++ R/gravitational_energy.R
@@ -0,0 +1,20 @@
+#' Gravitational Energy
+#'
+#' calculate the energy possessed or acquired by an object due to a change in its position when it is present in a gravitational field  = m*g*h
+#'
+#' @param m mass of the object (kg)
+#' @param g gravity of earth (m/s^2)
+#' @param h height above the earth surface (m)
+#'
+#' @return GE Gravitational energy of the object
+#' @export
+#'
+#' @examples
+#' m <- 2.5
+#' g <- 9.80665
+#' h <- 10
+#' gravitational_energy(m,g,h )
+#' 245.16
+gravitational_energy <- function(m,g=9.80665,h) {
+
+}
