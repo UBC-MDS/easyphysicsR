851f7424a60ccc5ebdb0fb62e066fc83e4d28371
Mengjun74 <91912728+Mengjun74@users.noreply.github.com>
2023-01-21 13:50:19 -0800

    Create static_friction_ground.R

---
+++ R/static_friction_ground.R
@@ -0,0 +1,26 @@
+#'  calculating the friction force for static object. The formula is fr = mu * N, where 
+#'  the mu is the coefficient of friction which incorporating the characteristics of the surface.
+#'  For example, if you want to calculate the friction when you put your phone on a table, then the coefficient of friction is 
+#'  depending on the matirial of the table. N is the normal force which means the perpendicular force of the surface which object 
+#'  touching on. In our case, it would be m*g since it is on the ground. fr = mu * m * g.
+#'
+#' @param mu
+#' A numeric value that is the coefficient of friction
+#'
+#' @param g 
+#' The gravity of the free fall.
+#'
+#' @param m
+#' A numeric value that is the mass of the object
+#' @return the static friction force
+#' @export
+#'
+#' @examples
+#' static_friction_ground(mu=0.2, g = 9.8, m=10)
+#' 19.6
+#' freefall(g=10, mu=0.2, m=2)
+#' 4
+#'
+static_friction_ground <- function(mu, m, g = 9.8) {
+  
+ }
