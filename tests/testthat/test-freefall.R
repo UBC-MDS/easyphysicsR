e5c8d4c20427f51162ef47d5984eb173ff52593e
YHuUBC <112221943+YHuUBC@users.noreply.github.com>
2023-02-02 17:57:06 -0800

    update test documentation for freefall

--- tests/testthat/test-freefall.R
+++ tests/testthat/test-freefall.R
@@ -1,15 +1,16 @@
+# test if freefall function works with the default g of 9.8
 test_that("freefall works with default g", {
   expect_equal(round(freefall(10)[[1]],4), 1.4286)
 })
-
+# test if freefall function works with a small g as a numeric vecctor
 test_that("freefall works with a small g", {
   expect_equal(round(freefall(100, g = 1)[[1]],3), 14.142)
 })
-
+# test if freefall function works with a large g as a numeric vector
 test_that("freefall works with a large g", {
   expect_equal(round(freefall(5, g = 100)[[1]],4), 0.3162)
 })
-
+# test if an error raises when input height or g as non-numeric type of data
 test_that("it is errorenous to input non-numeric data(
           e.g., string, list) for height or g", {
             expect_error(
@@ -19,7 +20,7 @@ test_that("it is errorenous to input non-numeric data(
             expect_error(
               freefall(tibble('planet')))
           })
-
+# test if the freefall function returns a numeric vector and a plot
 test_that("the function returns a numeric vector and a plot", {
   expect_true(is_list(freefall(10))
               & is.ggplot(freefall(
